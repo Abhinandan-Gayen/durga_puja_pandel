94c935acf9e8127fe3c2bcb73bee4c2ac66bb957
Madhab-Shit <madhabshit4142@gmail.com>
2026-08-01 07:52:14 +0530

    bottom screen

--- lib/views/Users/bottom_naigation.dart
+++ lib/views/Users/bottom_naigation.dart
@@ -1,4 +1,3 @@
-import 'package:durga_puja_pandel/core/theme/normal_color.dart';
 import 'package:durga_puja_pandel/views/Users/Explore.dart';
 import 'package:durga_puja_pandel/views/Users/dashbord.dart';
 import 'package:durga_puja_pandel/views/Users/favoutiry.dart';
@@ -8,50 +7,84 @@ import 'package:flutter/material.dart';
 
 class AppShell extends StatefulWidget {
   const AppShell({super.key});
+
   @override
   State<AppShell> createState() => _AppShellState();
 }
 
 class _AppShellState extends State<AppShell> {
-  int index = 0;
-  final saved = <int>{0, 2};
   int selectedBottomIndex = 0;
+
+  final Set<int> saved = {0, 2};
+
   static const Color primaryRed = Color(0xFFB91419);
+
+  void _toggleSaved(int value) {
+    setState(() {
+      if (saved.contains(value)) {
+        saved.remove(value);
+      } else {
+        saved.add(value);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final screens = [
-      // HomeScreen(onExplore: () => setState(() => index = 1)),
-      DurgaPujaHomeScreen(),
-      ExploreScreen(saved: saved, onSaved: _toggleSaved),
+    final List<Widget> screens = [
+      const DurgaPujaHomeScreen(),
+
+      ExploreScreen(
+        saved: saved,
+        onSaved: _toggleSaved,
+      ),
+
       const MapScreen(),
-      FavouritesScreen(saved: saved, onSaved: _toggleSaved),
+
+      FavouritesScreen(
+        saved: saved,
+        onSaved: _toggleSaved,
+      ),
+
       const ProfileScreen(),
     ];
+
     return Scaffold(
+      backgroundColor: const Color(0xFFFFF8E9),
+
       body: SafeArea(
+        bottom: false,
         child: Center(
           child: ConstrainedBox(
             constraints: const BoxConstraints(maxWidth: 520),
-            child: IndexedStack(index: index, children: screens),
+            child: IndexedStack(
+              index: selectedBottomIndex,
+              children: screens,
+            ),
           ),
         ),
       ),
+
       bottomNavigationBar: _buildBottomNavigation(),
     );
   }
 
-  void _toggleSaved(int value) => setState(() {
-    saved.contains(value) ? saved.remove(value) : saved.add(value);
-  });
-
   Widget _buildBottomNavigation() {
     final List<Map<String, dynamic>> items = [
-      {'icon': Icons.map_outlined, 'selectedIcon': Icons.map, 'title': 'Map'},
       {
-        'icon': Icons.near_me_outlined,
-        'selectedIcon': Icons.near_me,
-        'title': 'Nearby',
+        'icon': Icons.home_outlined,
+        'selectedIcon': Icons.home_rounded,
+        'title': 'Home',
+      },
+      {
+        'icon': Icons.explore_outlined,
+        'selectedIcon': Icons.explore_rounded,
+        'title': 'Explore',
+      },
+      {
+        'icon': Icons.map_outlined,
+        'selectedIcon': Icons.map_rounded,
+        'title': 'Map',
       },
       {
         'icon': Icons.favorite_border_rounded,
@@ -59,66 +92,92 @@ class _AppShellState extends State<AppShell> {
         'title': 'Saved',
       },
       {
-        'icon': Icons.route_outlined,
-        'selectedIcon': Icons.route,
-        'title': 'Routes',
+        'icon': Icons.person_outline_rounded,
+        'selectedIcon': Icons.person_rounded,
+        'title': 'Profile',
       },
     ];
 
-    return Container(
-      height: 62,
-      margin: const EdgeInsets.fromLTRB(8, 0, 8, 7),
-      decoration: BoxDecoration(
-        color: const Color(0xFFFFFCF5),
-        borderRadius: BorderRadius.circular(21),
-        border: Border.all(color: const Color(0xFFECDCC9), width: 0.8),
-        boxShadow: const [
-          BoxShadow(
-            color: Color(0x25000000),
-            blurRadius: 10,
-            offset: Offset(0, 3),
+    return SafeArea(
+      top: false,
+      child: Container(
+        height: 66,
+        margin: const EdgeInsets.fromLTRB(8, 0, 8, 7),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFFCF5),
+          borderRadius: BorderRadius.circular(21),
+          border: Border.all(
+            color: const Color(0xFFECDCC9),
+            width: 0.8,
           ),
-        ],
-      ),
-      child: Row(
-        children: List.generate(items.length, (index) {
-          final item = items[index];
-          final bool isSelected = selectedBottomIndex == index;
-
-          return Expanded(
-            child: InkWell(
-              onTap: () {
-                setState(() {
-                  selectedBottomIndex = index;
-                });
-              },
-              borderRadius: BorderRadius.circular(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    isSelected
-                        ? item['selectedIcon'] as IconData
-                        : item['icon'] as IconData,
-                    color: isSelected ? primaryRed : const Color(0xFF746B66),
-                    size: 20,
-                  ),
-                  const SizedBox(height: 3),
-                  Text(
-                    item['title'] as String,
-                    style: TextStyle(
-                      color: isSelected ? primaryRed : const Color(0xFF554D48),
-                      fontSize: 8,
-                      fontWeight: isSelected
-                          ? FontWeight.w700
-                          : FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0x25000000),
+              blurRadius: 10,
+              offset: Offset(0, 3),
             ),
-          );
-        }),
+          ],
+        ),
+        child: Row(
+          children: List.generate(items.length, (itemIndex) {
+            final Map<String, dynamic> item = items[itemIndex];
+
+            final bool isSelected =
+                selectedBottomIndex == itemIndex;
+
+            return Expanded(
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    selectedBottomIndex = itemIndex;
+                  });
+                },
+                borderRadius: BorderRadius.circular(20),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 220),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 7,
+                  ),
+                  decoration: BoxDecoration(
+                    color: isSelected
+                        ? primaryRed.withOpacity(0.08)
+                        : Colors.transparent,
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        isSelected
+                            ? item['selectedIcon'] as IconData
+                            : item['icon'] as IconData,
+                        color: isSelected
+                            ? primaryRed
+                            : const Color(0xFF746B66),
+                        size: 21,
+                      ),
+
+                      const SizedBox(height: 3),
+
+                      Text(
+                        item['title'] as String,
+                        style: TextStyle(
+                          color: isSelected
+                              ? primaryRed
+                              : const Color(0xFF554D48),
+                          fontSize: 8.5,
+                          fontWeight: isSelected
+                              ? FontWeight.w700
+                              : FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
