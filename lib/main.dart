8b400bdae9abd5464cceb67e72ac6cbd62215ca1
Madhab-Shit <madhabshit4142@gmail.com>
2026-08-02 21:18:15 +0530

    Explore change

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox<dynamic>('favoritePandals');
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const PujoPandalGuideApp());
 }
